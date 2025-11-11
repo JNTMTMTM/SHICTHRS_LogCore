fff7148a7a16877a999ba25d1908425b6d3b9252
JNTMTMTM <15251865468@163.com>
2025-11-11 19:29:57 +0800

    V0.0.2 优化SHRLogCore模块：重构日志输出格式，添加错误处理及调用栈信息

--- src/SHRLogCore.py
+++ src/SHRLogCore.py
@@ -1,14 +1,13 @@
 
 import sys , os
-sys.path.append('..')
-
-from utils.config.SHRLogCore_readConfigFile import read_config_file
-
+import inspect
+import logging
 from colorama import init
+sys.path.append('..')
 init()
 
-import logging
-
+from utils.config.SHRLogCore_readConfigFile import read_config_file
+from utils.time.SHRLogCore_pytzTimeSynchronizer import sync_system_time
 
 class SHRLogCore():
     def __init__(self):
@@ -18,14 +17,28 @@ class SHRLogCore():
     
     def __init_SHRLogCoreConfigSettings(self):
         if os.path.exists(os.path.join(self._EXEPATH , 'config' , 'SHRLogCoreConfigSettings.ini')):
-            self._SHRLogCoreConfigSettings = read_config_file(os.path.join(self._EXEPATH , 'config' , 'SHRLogCoreConfigSettings.ini'))
+            try:
+                self._SHRLogCoreConfigSettings = read_config_file(os.path.join(self._EXEPATH , 'config' , 'SHRLogCoreConfigSettings.ini'))
+                self.__outputLogsInConsole('DEBUG' , 'SHRLogCoreConfigSettings.ini 文件读取成功')
+            except:
+                self.__outputLogsInConsole('CRITICAL' , 'SHRLogCoreConfigSettings.ini 文件读取失败')
         else:
-            pass
+            self.__outputLogsInConsole('CRITICAL' , 'SHRLogCoreConfigSettings.ini 文件丢失')
     
-    def __outputLogsInConsole(self , base , log_level : str , log_message : str):
+    def __outputLogsInConsole(self , log_level : str , log_message : str):
+        """
+        LOG_LEVEL_COLOR_CPT | LOG-LEVELS
+        --------------------|-------------
+        GREEN               | DEBUG
+        BLUE                | INFO
+        YELLOW              | WARNING
+        RED                 | ERROR
+        MAGENTA             | CRITICAL
+        """
+
         LOG_LEVEL_COLOR_CPT : dict = {'DEBUG' : '\033[32m' , 'INFO' : '\033[34m' , 'WARNING' : '\033[33m' , 'ERROR' : '\033[31m' , 'CRITICAL' : '\033[35m'}
         END_COLOR : str = '\033[0m'
+        temp_frame = inspect.currentframe()
+        print(f'\033[1m{sync_system_time()}\033[0m {LOG_LEVEL_COLOR_CPT[log_level]}[{log_level}] {temp_frame.f_back.f_code.co_name} {END_COLOR}: {log_message}')
 
-        print(f'{LOG_LEVEL_COLOR_CPT[log_level]}[{log_level}] {base} : {log_message}{END_COLOR}')
-
-# print(read_config_file(os.path.join(os.getcwd() , 'config' , 'SHRLogCoreConfigSettings.ini')))
+log = SHRLogCore()
