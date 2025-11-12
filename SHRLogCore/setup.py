79af9caf2effa26af27dc2efea7fe084f01e3844
JNTMTMTM <15251865468@163.com>
2025-11-12 19:09:05 +0800

    V1.0.0 构建发布版本：添加setup.py安装配置，生成egg-info元数据文件，完善项目打包支持

---
+++ SHRLogCore/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='SHRLogCore',
+      version='1.0.0',
+      description='SHICTHRS LOG CORE logging system',
+      url='https://github.com/JNTMTMTM/SHICTHRS_LogCore',
+      author='SHICTHRS',
+      author_email='contact@shicthrs.com',
+      license='GPL-3.0',
+      zip_safe=False)
