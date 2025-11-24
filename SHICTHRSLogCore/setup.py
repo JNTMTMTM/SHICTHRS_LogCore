b84fe3dc115f1e33a3e8e92e93bd43afc91a0687
JNTMTMTM <15251865468@163.com>
2025-11-24 21:53:20 +0800

    V1.6.0 更新依赖版本：将SHICTHRSConfigLoader依赖从1.1.0升级至1.2.0

--- SHICTHRSLogCore/setup.py
+++ SHICTHRSLogCore/setup.py
@@ -9,5 +9,5 @@ setup(name='SHICTHRSLogCore',
       license='GPL-3.0',
       packages=find_packages(),
       include_package_data=True,
-      install_requires=['colorama==0.4.6' , 'pytz==2025.2' , 'SHICTHRSConfigLoader==1.1.0'],
+      install_requires=['colorama==0.4.6' , 'pytz==2025.2' , 'SHICTHRSConfigLoader==1.2.0'],
       zip_safe=False)
